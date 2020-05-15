312000783d88a05a944671c3d998f661fc616dda
saloni jain <jsaloni484@gmail.com>
2020-05-15 13:15:36 +0530

    include function

--- Day 2/Loops.js
+++ Day 2/Loops.js
@@ -1,24 +1,16 @@
-/*
- * Complete the vowelsAndConsonants function.
- * Print your output using 'console.log()'.
- */
-function vowelsAndConsonants(s) 
-{
-    var vowels = ['a', 'e', 'i', 'o', 'u']
-    for (var i = 0; i < s.length; i++)
-        {
-            if (vowels.indexOf(s[i]) > -1)
-                {
-                    console.log(s[i]);
-                }
+function vowelsAndConsonants(s) {
+ 
+    const vowels = 'aeiou';
+        var consonants = '';
+        
+        for(var i = 0; i < s.length; i++) {
+           if (vowels.includes(s[i])) {
+               console.log(s[i]);
+           }
+           else {
+               consonants += s[i] + '\n';
+           }
         }
-    
-    for (var j = 0; j < s.length; j++)
-        {
-            if (vowels.indexOf(s[j]) < 0)
-                {
-                    console.log(s[j]);
-                }
-        }
-    
-}
+        
+        console.log(consonants.trim());
+    }
